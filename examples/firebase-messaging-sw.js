0b7c8a053866f5a16a87b507f5d822fb777b60b0
Yohanes Gultom <yohanes.gultom@gmail.com>
2019-08-16 16:28:43 +0700

    remove background notification handler for web login example

--- examples/firebase-messaging-sw.js
+++ examples/firebase-messaging-sw.js
@@ -1,5 +1,13 @@
 // This file is required by firebase.messaging()
 // The filename has to be exactly the same and located at root eg. https://example.com/firebase-messaging-sw.js
+// or registered before enabling notification eg:
+//
+// navigator.serviceWorker.register('firebase-messaging-sw.js')
+//   .then((registration) => {
+//     messaging.useServiceWorker(registration)
+//     enableNotification()
+//   })
+
 
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
@@ -11,26 +19,4 @@ importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js')
 // messagingSenderId.
 firebase.initializeApp({
     'messagingSenderId': '371521361553'
-})
-
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging()
-
-// accept message when on background
-messaging.setBackgroundMessageHandler(function (payload) {
-    console.log('[firebase-messaging-sw.js] Received background message ', payload)
-    const notificationTitle = payload.data.title
-    const notificationOptions = {
-        body: payload.data.body,
-        icon: payload.data.icon,
-    }
-
-    // click handler
-    self.addEventListener('notificationclick', function (event) {
-        event.notification.close()
-        event.waitUntil(self.clients.openWindow(payload.data.url))
-    })
-        
-    return self.registration.showNotification(notificationTitle, notificationOptions)
 })
