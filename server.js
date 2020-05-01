095da2df9edce1a88e8dc58e86ff234cb46993a2
Saggaf Arsyad <saggaf.arsyad@gmail.com>
2020-05-02 02:17:11 +0700

    feat(server): added server util function; fix error and not found handlers; change MobileController router params

--- server.js
+++ server.js
@@ -1,61 +1,196 @@
 'use strict'
 
 const express = require('express'),
-	bodyParser = require('body-parser'),
-	path = require('path'),
-	WebConsoleController = require('./controllers/webconsole'),
-	MobileController = require('./controllers/mobile'),
-	WebController = require('./controllers/web'),
-	DemoAppController = require('./controllers/demo-app'),
-	ResponseComponent = require('./components/response')
+    bodyParser = require('body-parser'),
+    path = require('path'),
+    WebConsoleController = require('./controllers/webconsole'),
+    MobileController = require('./controllers/mobile'),
+    WebController = require('./controllers/web'),
+    DemoAppController = require('./controllers/demo-app'),
+    ResponseComponent = require('./components/response'),
+    APIError = require('./server/api_error')
+
+// Get Standard Codes
+const {STD_CODES} = ResponseComponent
 
 class Server {
-	constructor(models, common, middlewares, nexmo) {
-		// Init config
-		this.config = common.config
+    constructor(models, common, middlewares, nexmo) {
+        // Init config
+        this.config = common.config
 
-		// Init work dir
-		this.workDir = path.resolve('./')
+        // Init work dir
+        this.workDir = path.resolve('./')
 
-		// Init components
-		this.components = {
-			common,
-			nexmo,
-			response: new ResponseComponent({filePath: this.workDir + '/response-codes.json'})
-		}
+        // Init models
+        this.models = models
 
-		// Init models
-		this.models = models
+        // Init components
+        this.components = {
+            common,
+            nexmo,
+            response: new ResponseComponent({filePath: this.workDir + '/response-codes.json'})
+        }
 
-		this.app = express()
-		this.app.use(bodyParser.json())
-		this.app.use(bodyParser.urlencoded({ extended: true }))
+        // Init router
+        this.initRouter(middlewares)
+    }
 
-		// routes
-		this.app.use('/', express.static('doc'))
-		this.app.use('/lib', express.static('client/dist'))
-		this.app.use('/examples', express.static('examples'))
-		this.app.use('/console', new WebConsoleController(models, common, middlewares).router)
-		this.app.use('/mobile',  new MobileController(models, common, middlewares, nexmo).router)
-		this.app.use('/web', new WebController(models, common, nexmo).router)
-		this.app.use('/demo-app/api', new DemoAppController(models, common).router)
+    initRouter(middlewares) {
+        // Init router params
+        const routerParams = {
+            components: this.components,
+            config: this.config,
+            models: this.models,
+            server: {
+                handleAsync: this.handleAsync,
+                handleRESTAsync: this.handleRESTAsync,
+                sendResponse: this.sendResponse
+            }
+        }
 
-		// global error handler
-		this.app.use(function (err, req, res, next) {
-			console.error(err)
-			if (err.name === 'SequelizeValidationError' || err.name === 'ValidationError') {
-				return res.status(400).send(err.message)
-			} else {
-				return res.status(500).send(err.message)
-			}
-		})
+        // Get params
+        const models = this.models
+        const {common, nexmo} = this.components
 
-		// 404 handler
-		this.app.use(function(req, res){
-			res.status(404).send({ error: 'Unknown method' })
-		})
+        // Init Main Routers
+        this.app = express()
 
-	}
+        // Configure middlewares
+        this.app.use(bodyParser.json())
+        this.app.use(bodyParser.urlencoded({extended: true}))
+
+        // Configure routing
+        this.app.use('/', express.static('doc'))
+        this.app.use('/lib', express.static('client/dist'))
+        this.app.use('/examples', express.static('examples'))
+        this.app.use('/console', new WebConsoleController(models, common, middlewares).router)
+        this.app.use('/mobile', new MobileController(routerParams).router)
+        this.app.use('/web', new WebController(models, common, nexmo).router)
+        this.app.use('/demo-app/api', new DemoAppController(models, common).router)
+
+        // Handle Errors
+        this.app.use((req, res) => {
+            this.sendErrorResponse(res, new APIError(STD_CODES.ERROR_NOT_FOUND))
+        })
+
+        // Handle Resource Not Found
+        this.app.use((err, req, res) => {
+            this.sendErrorResponse(res, err)
+        })
+    }
+
+    /**
+     * Send success response
+     *
+     * @param res {*} Express response object
+     * @param opt {object} Options
+     * @param opt.message {string} Message to override
+     * @param opt.data {*} Data
+     * @param opt.code {string} Response code
+     */
+    sendResponse = (res, opt = {}) => {
+        // Get response component
+        const {response: responseComponent} = this.components
+
+        // Deconstruct options
+        const {code, message, data} = opt
+
+        /** @type {Response} */
+        let resp
+
+        if (code) {
+            // If code is set, get response code
+            resp = responseComponent.get(code, {success: true})
+        } else {
+            // Else, get a generic success
+            resp = responseComponent.getSuccess()
+        }
+
+        // Compose response
+        const body = resp.compose({message, data})
+
+        // Send response
+        res.json(body)
+    }
+
+    /**
+     * Send error response
+     *
+     * @param res {*} Express response
+     * @param err {APIError|Error} Error
+     */
+    sendErrorResponse = (res, err) => {
+        // Get response component
+        const {response: responseComponent} = this.components
+
+        /** @type Response */
+        let resp
+        /** @type any */
+        let data
+
+        // If error is not APIError, convert to Internal Error
+        if (err.constructor.name !== "APIError") {
+            resp = responseComponent.getInternalError()
+
+            // If debug mode, add source error stack
+            if (this.config.APP_DEBUG) {
+                data = {
+                    _errorDebug: {
+                        name: err.name,
+                        message: err.message,
+                        stack: err.stack
+                    }
+                }
+            }
+
+            // TODO: Log error cause
+        } else {
+            resp = responseComponent.get(err.code)
+        }
+
+        // Compose body
+        const body = resp.compose({data, message: err.message})
+
+        // Send response
+        res
+            .status(resp.status)
+            .json(body)
+    }
+
+    /**
+     * Wraps a Promised-based handler function and returns an express handler
+     *
+     * @param handlerFn
+     * @returns {function(...[*]=)}
+     */
+    handleAsync = handlerFn => {
+        // Create function
+        return async (req, res, next) => {
+            try {
+                await handlerFn(req, res, next)
+            } catch (err) {
+                this.sendErrorResponse(res, err)
+            }
+        }
+    }
+
+    /**
+     * Wraps a Async REST Handler function, receive result and send response
+     *
+     * @param {RESTHandlerAsyncFn} handlerFn
+     * @returns {function(...[*]=)} Express handler function
+     */
+    handleRESTAsync = handlerFn => {
+        // Create function
+        return async (req, res) => {
+            try {
+                const result = await handlerFn(req)
+                this.sendResponse(res, result)
+            } catch (err) {
+                this.sendErrorResponse(res, err)
+            }
+        }
+    }
 }
 
 module.exports = Server
