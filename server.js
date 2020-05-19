dc2d514d1290b6b30edf1283a3118db85c39e2fa
Saggaf Arsyad <saggaf@nusantarabetastudio.com>
2020-05-19 12:30:04 +0700

    doc: fix document routing

--- server.js
+++ server.js
@@ -92,13 +92,14 @@ class Server {
         this.app.use(bodyParser.urlencoded({extended: true}))
 
         // Configure routing
-        this.app.use(`${basePath}/`, express.static('doc'))
         this.app.use(`${basePath}/lib`, express.static('client/dist'))
         this.app.use(`${basePath}/examples`, express.static('examples'))
         this.app.use(`${basePath}/console`, new WebConsoleController(models, common, legacyMiddlewares).router)
         this.app.use(`${basePath}/mobile`, new MobileController(routerParams).router)
         this.app.use(`${basePath}/server`, new ServerController(routerParams).router)
         this.app.use(`${basePath}/web`, new WebController(models, common, nexmo).router)
+        this.app.use(`${basePath}/`, express.static('doc'))
+        this.app.use(`${basePath}/vendor`, express.static('doc/vendor'))
 
         // Handle Errors
         this.app.use((req, res) => {
