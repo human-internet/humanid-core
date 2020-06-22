15092411885c70bfdf8cc3db1fc0989e5df97102
saggaf <saggaf@nusantarabetastudio.com>
2020-06-22 23:10:33 +0700

    feat(db): remove legacy tables, added migration of db design rev 3

---
+++ migrations/sequential/20200622155000-create-user.js
@@ -0,0 +1,94 @@
+'use strict'
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        // Create reference data table
+        await queryInterface.createTable('UserStatus', {
+            id: {
+                type: Sequelize.INTEGER,
+                primaryKey: true,
+                autoIncrement: false,
+            },
+            name: {
+                type: Sequelize.STRING(32),
+                allowNull: false
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: '2020-01-01 00:00:00'
+            }
+        })
+
+        // Create table
+        await queryInterface.createTable('User', {
+            id: {
+                type: Sequelize.BIGINT,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            hashId: {
+                type: Sequelize.STRING(256),
+                allowNull: false
+            },
+            hashIdVersion: {
+                type: Sequelize.BIGINT,
+                allowNull: false,
+                defaultValue: 1
+            },
+            hashIdFormatVersion: {
+                type: Sequelize.BIGINT,
+                allowNull: false,
+                defaultValue: 1
+            },
+            recoveryHashId: {
+                type: Sequelize.STRING(256),
+                allowNull: true
+            },
+            recoveryHashIdVersion: {
+                type: Sequelize.BIGINT,
+                allowNull: true
+            },
+            recoveryHashIdFormatVersion: {
+                type: Sequelize.BIGINT,
+                allowNull: true
+            },
+            countryCode: {
+                type: Sequelize.STRING(3),
+                allowNull: false
+            },
+            userStatusId: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'UserStatus',
+                    key: 'id'
+                }
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: '2020-01-01 00:00:00'
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: '2020-01-01 00:00:00'
+            }
+        })
+
+        // Create index
+        await queryInterface.addIndex('User', ['hashId'], {indicesType: "UNIQUE"})
+
+        // Insert reference data
+        await queryInterface.bulkInsert('UserStatus', [
+            {id: 1, name: 'Unverified', updatedAt: '2020-01-01 00:00:00'},
+            {id: 2, name: 'Verified', updatedAt: '2020-01-01 00:00:00'},
+            {id: 3, name: 'Suspended', updatedAt: '2020-01-01 00:00:00'}
+        ])
+    },
+    down: async queryInterface => {
+        await queryInterface.dropTable('User')
+        await queryInterface.dropTable('UserStatus')
+    }
+}
---
+++ migrations/sequential/20200622223400-create-user-otp.js
@@ -0,0 +1,109 @@
+'use strict'
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        // Create reference data table
+        await queryInterface.createTable('UserOTPSession', {
+            id: {
+                type: Sequelize.BIGINT,
+                primaryKey: true,
+                autoIncrement: false,
+            },
+            appUserId: {
+                type: Sequelize.BIGINT,
+                allowNull: false,
+                references: {
+                    model: 'AppUser',
+                    key: 'id'
+                }
+            },
+            rule: {
+                type: Sequelize.JSON,
+                allowNull: false
+            },
+            otpCount: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            failAttemptCount: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            expiredAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: '2020-01-01 00:00:00'
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: '2020-01-01 00:00:00'
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: '2020-01-01 00:00:00'
+            },
+            version: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: '2020-01-01 00:00:00'
+            }
+        })
+
+        // Create table
+        await queryInterface.createTable('UserOTP', {
+            id: {
+                type: Sequelize.BIGINT,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            sessionId: {
+                type: Sequelize.BIGINT,
+                allowNull: false,
+                references: {
+                    model: 'UserOTPSession',
+                    key: 'id'
+                }
+            },
+            otpNo: {
+                type: Sequelize.INTEGER,
+                allowNull: false
+            },
+            requestId: {
+                type: Sequelize.STRING(64),
+                allowNull: false
+            },
+            signature: {
+                type: Sequelize.STRING(256),
+                allowNull: false
+            },
+            metadata: {
+                type: Sequelize.JSON,
+                allowNull: false
+            },
+            expiredAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: '2020-01-01 00:00:00'
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: '2020-01-01 00:00:00'
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: '2020-01-01 00:00:00'
+            }
+        })
+
+        // Create index
+        await queryInterface.addIndex('UserOTP', ['requestId'], {indicesType: "UNIQUE"})
+    },
+    down: async queryInterface => {
+        await queryInterface.dropTable('UserOTP')
+        await queryInterface.dropTable('UserOTPSession')
+    }
+}
