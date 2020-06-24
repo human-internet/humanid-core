3d17e00efd4915240c581d7bbb6599d9a2f2121f
Saggaf Arsyad <saggaf@nusantarabetastudio.com>
2020-06-24 19:58:16 +0700

    doc: added Request OTP documentation for version v0.0.3

--- controllers/mobile-doc.js
+++ controllers/mobile-doc.js
@@ -42,6 +42,54 @@
  * @apiUse ErrorResponse
  */
 
+/**
+ * @api {post} /mobile/users/login/request-otp Login: Request OTP
+ * @apiVersion 0.0.3
+ * @apiName RequestLoginOtp
+ * @apiGroup Core.MobileAPI
+ * @apiDescription Trigger send OTP code via SMS
+ *
+ * @apiUse MobileClientCredential
+ * @apiParam {String} countryCode User mobile phone country code (eg. 62 for Indonesia)
+ * @apiParam {String} phone User mobile phone number
+ *
+ * @apiUse SuccessResponse
+ * @apiUse OkResponseExample
+ * @apiSuccess {Object} data Response data
+ * @apiSuccess {String} data.requestId OTP Request ID
+ * @apiSuccess {String} data.nextResendAt Timestamp in Unix Epoch that indicates when resend OTP is allowed
+ * @apiSuccess {String} data.failAttemptCount Failed verification input counter
+ * @apiSuccess {String} data.otpCount Resend counter
+ * @apiSuccess {Object} data.config OTP Configuration
+ * @apiSuccess {Object} data.config.otpSessionLifetime OTP Session lifetime in second
+ * @apiSuccess {Object} data.config.otpCountLimit Resend limit
+ * @apiSuccess {Object} data.config.failAttemptLimit Failed verification attempt limit
+ * @apiSuccess {Object} data.config.nextResendDelay OTP allowed resend delay in second
+ * @apiSuccess {Object} data.config.otpCodeLength OTP Code Length
+ * @apiSuccessExample {json} SuccessResponse:
+ *   {
+ *     "success": true,
+ *     "code": "OK",
+ *     "message": "Success",
+ *     "data": {
+ *         "requestId": "E9NETW9R34Z0S3OVHK6NMDYA",
+ *         "nextResendAt": 1593003113,
+ *         "failAttemptCount": 0,
+ *         "otpCount": 1,
+ *         "config": {
+ *             "otpSessionLifetime": 300,
+ *             "otpCountLimit": 3,
+ *             "failAttemptLimit": 5,
+ *             "nextResendDelay": 60,
+ *             "otpCodeLength": 4
+ *         }
+ *     }
+ * }
+ *
+ *
+ * @apiUse ErrorResponse
+ */
+
 // ----------
 
 /**
