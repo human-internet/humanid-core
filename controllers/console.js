f1ede2451bac239146d251a6c61279c086ecefdd
Saggaf Arsyad <saggaf.arsyad@gmail.com>
2020-08-03 09:41:03 +0700

    fix(dev-console): fix owner id param

--- controllers/console.js
+++ controllers/console.js
@@ -16,26 +16,6 @@ class ConsoleController extends BaseController {
         this.route()
     }
 
-    handleRegisterDevUser = this.handleRESTAsync(async req => {
-        const body = req.body
-        this.validate({
-            ownerEntityTypeId: 'required',
-            ownerId: 'required',
-            countryCode: 'required',
-            phone: 'required'
-        }, body)
-
-        // Register dev user
-        await this.services.App.registerDevUser({
-            ownerEntityTypeId: body.ownerEntityTypeId,
-            ownerId: body.ownerEntityTypeId,
-            inputCountryCode: body.countryCode,
-            inputPhoneNo: body.phone
-        })
-
-        return {}
-    })
-
     route() {
         this.router = express.Router()
 
@@ -166,6 +146,20 @@ class ConsoleController extends BaseController {
         }
 
         next()
+    }
+
+    route() {
+        this.router = express.Router()
+
+        this.router.get('/apps', this.handleConsoleAuth, this.handleListApp)
+        this.router.post('/apps', this.handleConsoleAuth, this.handleCreateApp)
+        this.router.delete('/apps/:appExtId', this.handleConsoleAuth, this.handleDeleteApp)
+        this.router.post('/apps/:appExtId/credentials', this.handleConsoleAuth, this.handleCreateAppCredential)
+        this.router.get('/apps/:appExtId/credentials', this.handleConsoleAuth, this.handleListAppCredential)
+        this.router.delete('/apps/:appExtId/credentials/:clientId', this.handleConsoleAuth, this.handleDeleteAppCredential)
+        this.router.put('/apps/:appExtId/credentials/:clientId/status', this.handleConsoleAuth, this.handleToggleAppCredentialStatus)
+        this.router.post('/sandbox/dev-users', this.handleConsoleAuth, this.handleRegisterDevUser)
+        this.router.get('/sandbox/dev-users', this.handleConsoleAuth, this.handleListDevUser)
     }
 }
 
