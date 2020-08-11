9849ef74d44d9b22bf6124234278215c276e7334
Saggaf Arsyad <saggaf.arsyad@gmail.com>
2020-08-11 18:02:19 +0700

    added English and Indonesia localization message for OTP request message

---
+++ server/localization.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const Localization = {}
+
+Localization.OTP_REQUEST_MESSAGE = {
+    'en': 'Your humanID verification code is {OTP_CODE}',
+    'id': 'Kode verifikasi humanID Anda adalah {OTP_CODE}'
+}
+
+
+module.exports = Localization
