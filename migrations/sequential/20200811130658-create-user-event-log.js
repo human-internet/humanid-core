2b2638d916f6d09980c061fb7e98cd9caa83d61d
Saggaf Arsyad <saggaf.arsyad@gmail.com>
2020-08-12 11:59:47 +0700

    add migration to create UserEventLog table

---
+++ migrations/sequential/20200811130658-create-user-event-log.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('UserEventLog', {
+            id: {
+                type: Sequelize.BIGINT,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            ownerId: {
+                type: Sequelize.STRING(64),
+                allowNull: false
+            },
+            appId: {
+                type: Sequelize.BIGINT,
+                allowNull: false
+            },
+            appSnapshot: {
+                type: Sequelize.JSON,
+                allowNull: false
+            },
+            userFingerprint: {
+                type: Sequelize.STRING(64),
+                allowNull: false
+            },
+            eventName: {
+                type: Sequelize.STRING(64),
+                allowNull: false
+            },
+            metadata: {
+                type: Sequelize.JSON,
+                allowNull: true
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: '2020-01-01 00:00:00'
+            }
+        })
+
+        await queryInterface.addIndex('UserEventLog', ['ownerId', 'appId', 'eventName', 'userFingerprint', 'createdAt'],
+            {
+                name: 'idx_user_event'
+            })
+    },
+
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('UserEventLog')
+    }
+};
