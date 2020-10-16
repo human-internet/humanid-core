dcee03a04419b7a5a18ded80251d20682cd5347f
Saggaf Arsyad <saggaf.arsyad@gmail.com>
2020-10-16 08:35:26 +0700

    feat(app): added console and json logging format

--- logger.js
+++ logger.js
@@ -1,14 +1,42 @@
 'use strict'
 
 const winston = require('winston');
+const {format} = winston
+
 
 // Retrieve logger configuration from env
 const logLevel = process.env['LOG_LEVEL'] || 'info'
+const logFormat = process.env['LOG_FORMAT'] || 'json'
+
+// Determine format
+let logFmt
+if (logFormat === 'console') {
+    logFmt = format.combine(
+        format.timestamp({}),
+        format.colorize({
+            all: true
+        }),
+        format.printf((info) => {
+            // Set metadata
+            let metadata = ''
+            if (info.metadata) {
+                metadata = '\n' + JSON.stringify(info.metadata, null, 4)
+            }
+            return `[${info.timestamp}] ${info.level}:\t${info.message}${metadata}`
+        }),
+    )
+} else {
+    logFmt = format.combine(
+        format.timestamp(),
+        format.json(),
+    )
+}
+
 
 // Create logger singleton module
 module.exports = winston.createLogger({
     level: logLevel,
-    format: winston.format.json(),
+    format: logFmt,
     transports: [
         new winston.transports.Console({})
     ]
