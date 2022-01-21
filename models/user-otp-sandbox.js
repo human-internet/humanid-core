9793ea35b7ef16805d74d568ad55c6e179f0a9bd
Saggaf <saggaf.arsyad@gmail.com>
2022-01-21 10:38:13 +0700

    fix(otp): Replace raw query with sequelize orm

--- models/user-otp-sandbox.js
+++ models/user-otp-sandbox.js
@@ -8,49 +8,60 @@ const
     MODEL_NAME = 'UserOTPSandbox'
 
 module.exports = (sequelize) => {
-    const Model = sequelize.define(MODEL_NAME, {
-        id: {
-            type: Sequelize.BIGINT,
-            primaryKey: true,
-            autoIncrement: true
+    const Model = sequelize.define(
+        MODEL_NAME,
+        {
+            id: {
+                type: Sequelize.BIGINT,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            sessionId: {
+                type: Sequelize.BIGINT,
+                allowNull: false,
+            },
+            devUserId: {
+                type: Sequelize.BIGINT,
+                allowNull: false,
+            },
+            otpCode: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            expiredAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: "2020-01-01 00:00:00",
+            },
         },
-        sessionId: {
-            type: Sequelize.BIGINT,
-            allowNull: false
-        },
-        devUserId: {
-            type: Sequelize.BIGINT,
-            allowNull: false
-        },
-        otpCode: {
-            type: Sequelize.STRING,
-            allowNull: false
-        },
-        expiredAt: {
-            type: Sequelize.DATE,
-            allowNull: false
-        },
-        createdAt: {
-            type: Sequelize.DATE,
-            allowNull: false,
-            defaultValue: '2020-01-01 00:00:00'
+        {
+            tableName: TABLE_NAME,
+            timestamps: false,
         }
-    }, {
-        tableName: TABLE_NAME,
-        timestamps: false
-    })
+    );
 
     Model.associate = function (models) {
         Model.belongsTo(models.OrgDevUser, {
-            foreignKey: 'devUserId',
-            as: 'devUser',
-        })
+            foreignKey: "devUserId",
+            as: "devUser",
+        });
 
         Model.belongsTo(models.UserOTPSession, {
-            foreignKey: 'sessionId',
-            as: 'session',
-        })
-    }
+            foreignKey: "sessionId",
+            as: "session",
+        });
+    };
 
-    return Model
-}
+    Model.associate = function (models) {
+        Model.belongsTo(models.OrgDevUser, {
+            foreignKey: "devUserId",
+            as: "devUser",
+        });
+    };
+
+    return Model;
+};
