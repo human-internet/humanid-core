a080b2d70e0c229b4df41620c0d73a2d9ae7fe0d
Saggaf <saggaf.arsyad@gmail.com>
2022-03-24 11:12:20 +0700

    refactor: Update project structure, remove unused files and dependencies

--- components/index.js
+++ components/index.js
@@ -1,46 +1,42 @@
-'use strict'
+"use strict";
 
-const
-    Common = require('./common'),
-    DateUtil = require('./date_util'),
-    ResponseMapper = require('./response-mapper'),
-    smsNexmo = require('./nexmo'),
-    smsAWS = require('./aws-sms'),
-    smsVonage = require('./vonage-sms'),
-    logger = require('../logger')
+const Common = require("./common"),
+    DateUtil = require("./date_util"),
+    ResponseMapper = require("./response-mapper"),
+    smsAWS = require("./aws-sms"),
+    smsVonage = require("./vonage-sms"),
+    logger = require("../logger");
 
-function init({config}) {
+function init({ config }) {
     // Init response mapper singleton
-    ResponseMapper.init({filePath: config.server.workDir + '/response-codes.json'})
+    ResponseMapper.init({ filePath: config.server.workDir + "/response-codes.json" });
 
     // Init AWS SMS Provider
     smsAWS.init({
-        accessKeyId: config['AWS_ACCESS_KEY_ID'],
-        secretAccessKey: config['AWS_SECRET_ACCESS_KEY'],
-        region: config['AWS_SMS_REGION']
-    })
+        accessKeyId: config["AWS_ACCESS_KEY_ID"],
+        secretAccessKey: config["AWS_SECRET_ACCESS_KEY"],
+        region: config["AWS_SMS_REGION"],
+    });
 
     smsVonage.init({
-        apiKey: config['NEXMO_API_KEY'],
-        apiSecret: config['NEXMO_API_SECRET'],
-        senderIdDefault: config['NEXMO_SENDER_ID_DEFAULT'],
+        apiKey: config["NEXMO_API_KEY"],
+        apiSecret: config["NEXMO_API_SECRET"],
+        senderIdDefault: config["NEXMO_SENDER_ID_DEFAULT"],
         senderIdMap: {
-            'US': config['NEXMO_SENDER_ID_US'],
-            'VN': config['NEXMO_SENDER_ID_VN']
+            US: config["NEXMO_SENDER_ID_US"],
+            VN: config["NEXMO_SENDER_ID_VN"],
         },
-        logger: logger
-    })
+        logger: logger,
+    });
 
     // Return components
     return {
-        common: Common,
-        dateUtil: DateUtil,
-        nexmo: smsNexmo,
         response: ResponseMapper,
+        dateUtil: DateUtil,
+        common: Common,
         smsAWS: smsAWS,
-        smsVonage: smsVonage
-    }
+        smsVonage: smsVonage,
+    };
 }
 
-module.exports = {init}
-
+module.exports = { init };
