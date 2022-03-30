d8c6df79e84becccf54a30ac9b01633bd81b415a
Saggaf <saggaf.arsyad@gmail.com>
2022-03-30 22:57:38 +0700

    feat(account): Add API Set Account Recovery Email

--- src/server/middlewares.js
+++ src/server/middlewares.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const Constants = require("../constants");
+const APIError = require("./api_error");
 
 class Middlewares {
     constructor({ components, services, server, logger }) {
@@ -15,6 +16,22 @@ class Middlewares {
 
             // Call validation service
             req.client = await this.services.Auth.authClient(cred, Constants.AUTH_SCOPE_WEB_LOGIN);
+
+            next();
+        });
+
+        this.authWebLoginClient = this.server.handleAsync(async (req, res, next) => {
+            // Get credential
+            const cred = this.parseBasicAuthorization(req);
+
+            // Call validation service
+            req.client = await this.services.Auth.authClient(
+                {
+                    clientId: cred.userName,
+                    clientSecret: cred.password,
+                },
+                Constants.AUTH_SCOPE_WEB_LOGIN
+            );
 
             next();
         });
@@ -57,6 +74,39 @@ class Middlewares {
         );
 
         return cred;
+    }
+
+    parseBasicAuthorization(req) {
+        const value = req.headers.authorization;
+        if (!value) {
+            throw new APIError("400");
+        }
+
+        // Split string into 2
+        const tmp = value.split(" ", 2);
+        if (tmp.length !== 2) {
+            throw new APIError("400");
+        }
+
+        // If header not basic
+        if (tmp[0] !== "Basic") {
+            throw new APIError("401");
+        }
+
+        // Decode base64
+        const buff = Buffer.from(tmp[1], "base64");
+        const cred = buff.toString("utf-8");
+
+        // Get username and password
+        const userPass = cred.split(":", 2);
+        if (userPass.length !== 2) {
+            userPass[1] = "";
+        }
+
+        return {
+            userName: userPass[0],
+            password: userPass[1],
+        };
     }
 }
 
