7a3e0dc5d804f6497f053774174ae25d1fab3254
Saggaf <saggaf.arsyad@gmail.com>
2022-03-31 08:13:35 +0700

    Fix warnings

--- src/controllers/base.js
+++ src/controllers/base.js
@@ -56,45 +56,6 @@ class BaseController {
         }
         return null;
     }
-
-    /**
-     * Validate app credentials
-     * @param {*} appId
-     * @param {*} appSecret
-     * @param {*} app
-     */
-    async validateAppCredentials(appId, appSecret, app) {
-        if (!app) {
-            app = await this.models.LegacyApp.findByPk(appId);
-        }
-        if (!app || app.id !== appId) {
-            throw new Error(`Invalid app ID: ${appId}`);
-        }
-        if (app.secret !== appSecret) {
-            throw new Error(`Invalid secret: ${appSecret}`);
-        }
-        return app;
-    }
-
-    /**
-     * Validate app user credentials and return user object
-     * @param {*} hash
-     * @param {*} appId
-     * @param {*} appSecret
-     */
-    async validateAppUserCredentials(hash, appId, appSecret) {
-        // validate login hash
-        let appUser = await this.models.LegacyAppUser.findOne({
-            where: { hash: hash },
-            include: [{ all: true }],
-        });
-        if (!appUser) {
-            throw new Error(`Invalid login hash`);
-        }
-        // validate app credentials
-        this.validateAppCredentials(appId, appSecret, appUser.app);
-        return appUser;
-    }
 }
 
 module.exports = BaseController;
